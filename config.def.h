2c60f70b7a823a10ef7de03a2197feb00ea28390
Yeyito <alinerob31@gmail.com>
2025-10-12 02:02:05 -0500

    Checkpoint with simple whale rice

--- config.def.h
+++ config.def.h
@@ -5,9 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
-
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
@@ -96,32 +95,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#00050f", /* black → abyssal trench */
+	"#ff6b6b", /* red   → coral reef */
+	"#2ec4b6", /* green → seafoam teal */
+	"#ffd166", /* yellow→ sunlight sand */
+	"#090d35", /* blue  → deep navy */
+	"#9d4edd", /* magenta → purple kelp */
+	"#00b4d8", /* cyan  → aqua splash */
+	"#a8dadc", /* white → pale aqua mist */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#1d3557", /* bright black → slatey deep blue */
+	"#ff8787", /* bright red   → soft coral */
+	"#4ddbb7", /* bright green → tropical teal */
+	"#ffe066", /* bright yellow→ golden reflection */
+	"#5fa8d3", /* bright blue  → shallow wave */
+	"#c77dff", /* bright magenta → lavender tide */
+	"#48cae4", /* bright cyan  → lagoon aqua */
+	"#f1faee", /* bright white → foamy white */
 
 	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	/* extra colors (fg/bg/cursor) */
+	"#f1faee", /* 256 → default foreground (foam white) */
+	"#00050f", /* 257 → default background (abyss black) */
+	"#48cae4", /* 258 → cursor (tropical aqua) */
+	"#f1faee", /* 259 → reverse cursor (foam white) */
 };
 
 
@@ -129,10 +128,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
